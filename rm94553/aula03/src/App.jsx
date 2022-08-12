29cf00167097088d2dda154e493aa79daeed3aaf
igorcamargo1 <rm94553@fiap.com.br>
2022-08-12 12:06:07 -0300

    criando state

---
+++ rm94553/aula03/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function App(){
+    
+    const parag = {
+        color: 'pink',
+        fontFamily: 'Trebuchet',
+        fontSize:'10px',
+        letterSpacing:'5px',
+        textIndent:'50px'
+    }
+    
+    
+    return(
+        <div>
+            <h1 style ={{fontSize:'2em', color:'#FF0000'}}>INICIANDO ESTUDOS COM STATE</h1>
+            <div>
+                <p style = {parag}>As rosas são vermelhas, violetas são azuis!</p>
+            </div>
+        </div>
+    )
+}
